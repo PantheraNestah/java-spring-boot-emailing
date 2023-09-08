9e9543fe4fe7979fa0fefd7cac3160be823796a0
PantheraNestah <pantheranestah@gmail.com>
2023-09-08 12:43:43 +0300

    first commit

---
+++ src/main/java/com/SpringBootEmailing/EmailingApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBootEmailing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailingApplication.class, args);
+	}
+
+}
